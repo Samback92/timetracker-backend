58f9f6808f8b9d3d6e43d6cc065648a1d6f40109
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-08-25 15:36:13 +0200

    Update CorsConfig.java

--- src/main/java/timetracker_backend/timetracker_backend/Config/CorsConfig.java
+++ src/main/java/timetracker_backend/timetracker_backend/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://jellyfish-app-m6ju4.ondigitalocean.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
